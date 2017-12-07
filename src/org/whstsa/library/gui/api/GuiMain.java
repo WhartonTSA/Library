3962658b1cf518042a5952d3ee6966052ef1bb72
Eric Rabil <ericjrabil@gmail.com>
2017-12-07 13:09:21 -0500

    Move component-specific code to dedicated LibraryManagement class

--- src/org/whstsa/library/gui/api/GuiMain.java
+++ src/org/whstsa/library/gui/api/GuiMain.java
@@ -4,21 +4,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.layout.StackPane;
-import org.whstsa.library.api.Callback;
 import org.whstsa.library.api.impl.Person;
-import org.whstsa.library.api.impl.library.Library;
-import org.whstsa.library.db.Loader;
 import org.whstsa.library.db.ObjectDelegate;
 import org.whstsa.library.gui.components.Table;
-import org.whstsa.library.gui.dialogs.NewLibraryDialog;
+import org.whstsa.library.gui.components.tables.DatabaseManagementTables;
 import org.whstsa.library.gui.factories.GuiUtils;
 import org.whstsa.library.util.ArrayUtils;
 import org.whstsa.library.util.ClickHandler;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GuiMain implements Gui {
 
@@ -27,28 +22,7 @@ public class GuiMain implements Gui {
     @Override
     public Scene draw() {
 
-        Table<Library> libraryTable = new Table<>();
-        libraryTable.addColumn("Library Name", "name", true, TableColumn.SortType.DESCENDING, 100);
-        List<Library> libraryList = ArrayUtils.castList(ObjectDelegate.getLibraries(), new ArrayList<Library>());
-        libraryTable.addItems(libraryList);
-
-        Button newLibraryButton = GuiUtils.createButton("New Library", (event) -> {
-            NewLibraryDialog.getLibraryName((name) -> {
-                if (name == null) {
-                    return;
-                }
-                Library library = new Library(name);
-                Loader.getLoader().loadLibrary(library);
-                libraryTable.addItem(library);
-            });
-        });
-        Button editLibraryButton = GuiUtils.createButton("Edit Library", this.defaultClickConsumer);
-        Button deleteLibraryButton = GuiUtils.createButton("Delete Library", this.defaultClickConsumer);
-        Button openLibraryButton = GuiUtils.createButton("Open Library", this.defaultClickConsumer);
-
-        StackPane libraryButtonContainer = GuiUtils.createSplitPane(GuiUtils.Orientation.VERTICAL, newLibraryButton, editLibraryButton, deleteLibraryButton, openLibraryButton);
-
-        StackPane libraryContainer = GuiUtils.createSplitPane(GuiUtils.Orientation.HORIZONTAL, libraryButtonContainer, libraryTable.getTable());
+        StackPane libraryContainer = DatabaseManagementTables.libraryOverviewTable();
 
         Button newPersonButton = GuiUtils.createButton("New Person", this.defaultClickConsumer);
         Button editPersonButton = GuiUtils.createButton("Edit Person", this.defaultClickConsumer);
@@ -72,4 +46,5 @@ public class GuiMain implements Gui {
     public String getUUID() {
         return "GUI_MAIN";
     }
+
 }
