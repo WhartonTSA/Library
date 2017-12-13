aba71903100407297fae6c4ead5e902985a80012
Eric Rabil <ericjrabil@gmail.com>
2017-12-13 13:10:14 -0500

    Updated to use new result system

--- src/org/whstsa/library/gui/dialogs/LibraryMetaDialogs.java
+++ src/org/whstsa/library/gui/dialogs/LibraryMetaDialogs.java
@@ -5,7 +5,8 @@ import javafx.scene.control.Dialog;
 import org.whstsa.library.api.Callback;
 import org.whstsa.library.api.impl.library.Library;
 import org.whstsa.library.api.library.ILibrary;
-import org.whstsa.library.gui.api.InputGroup;
+import org.whstsa.library.gui.components.Element;
+import org.whstsa.library.gui.components.TextFieldElement;
 import org.whstsa.library.gui.factories.DialogUtils;
 import org.whstsa.library.gui.factories.GuiUtils;
 
@@ -16,23 +17,23 @@ public class LibraryMetaDialogs {
     private static final String LIBRARY_FIELD = "Library Name";
 
     public static void createLibrary(Callback<ILibrary> callback) {
-        Dialog<Map<String, String>> dialog = DialogUtils.createInputDialog(ButtonType.FINISH, true, "Please provide a library name", LIBRARY_FIELD);
+        Dialog<Map<String, Element>> dialog = DialogUtils.createInputDialog(ButtonType.FINISH, true, "Please provide a library name", GuiUtils.createTextField(LIBRARY_FIELD));
         DialogUtils.getDialogResults(dialog, (results) -> {
             if (results.get(LIBRARY_FIELD) == null) {
                 callback.callback(null);
                 return;
             }
-            ILibrary library = new Library(results.get(LIBRARY_FIELD));
+            ILibrary library = new Library((String) results.get(LIBRARY_FIELD).getResult());
             callback.callback(library);
         });
     }
 
     public static void updateLibrary(ILibrary library, Callback<ILibrary> callback) {
-        InputGroup libraryFieldGroup = GuiUtils.createInputGroup(LIBRARY_FIELD);
-        libraryFieldGroup.setValue(library.getName());
-        Dialog<Map<String, String>> dialog = DialogUtils.createInputDialog(ButtonType.FINISH, true, "Updating Library", libraryFieldGroup);
+        TextFieldElement libraryNameField = GuiUtils.createTextField(LIBRARY_FIELD);
+        libraryNameField.setText(library.getName());
+        Dialog<Map<String, Element>> dialog = DialogUtils.createInputDialog(ButtonType.FINISH, true, "Updating Library", libraryNameField);
         DialogUtils.getDialogResults(dialog, (results) -> {
-            String newName = results.get(LIBRARY_FIELD);
+            String newName = (String) results.get(LIBRARY_FIELD).getResult();
             if (newName == null) {
                 callback.callback(library);
                 return;
