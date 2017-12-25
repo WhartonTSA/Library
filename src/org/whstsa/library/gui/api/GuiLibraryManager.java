a526f1f0e972df490e8a491152d1e276ef3ddf04
Camnewb <camnewborn@gmail.com>
2017-12-24 22:16:49 -0500

    Added Library Manager Gui
    Added methods to GuiUtils for creating HBoxes, VBoxes, and BorderPanes
    Edited methods for creating labels and buttons for better customization
    Added buttons and layout for the Library Manager

--- src/org/whstsa/library/gui/api/GuiLibraryManager.java
+++ src/org/whstsa/library/gui/api/GuiLibraryManager.java
@@ -1,9 +1,13 @@
 package org.whstsa.library.gui.api;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import org.whstsa.library.gui.factories.GuiUtils;
 
 public class GuiLibraryManager implements Gui {
@@ -13,7 +17,25 @@ public class GuiLibraryManager implements Gui {
 
         TableView mainTable = new TableView();
 
-        StackPane mainContainer = GuiUtils.createSplitPane(GuiUtils.Orientation.HORIZONTAL, mainTable);
+        Label membersLabel = GuiUtils.createLabel("Members", 16);
+        Button memberNew = GuiUtils.createButton("New", GuiUtils.defaultClickHandler());
+        Button memberList = GuiUtils.createButton("List", GuiUtils.defaultClickHandler());
+        Button memberSearch = GuiUtils.createButton("Search", GuiUtils.defaultClickHandler());
+        Button memberDelete = GuiUtils.createButton("Delete", GuiUtils.defaultClickHandler());
+
+
+        Label booksLabel = GuiUtils.createLabel("Books", 16);
+        Button bookAdd = GuiUtils.createButton("Add", GuiUtils.defaultClickHandler());
+        Button bookList = GuiUtils.createButton("List", GuiUtils.defaultClickHandler());
+        Button bookDelete = GuiUtils.createButton("Delete", GuiUtils.defaultClickHandler());
+        Button bookSearch = GuiUtils.createButton("Search", GuiUtils.defaultClickHandler());
+
+        Button settingsButton = GuiUtils.createButton("Settings", GuiUtils.defaultClickHandler());
+
+        VBox buttonGroup = GuiUtils.createVBox(15, membersLabel, memberNew, memberList, memberSearch, memberDelete, booksLabel, bookAdd, bookList, bookDelete, bookSearch, settingsButton);
+        buttonGroup.setSpacing(5.0);
+
+        BorderPane mainContainer = GuiUtils.createBorderPane(GuiUtils.Direction.LEFTHAND, mainTable, buttonGroup);
 
         return new Scene(mainContainer, 1200, 800);
     }
--- src/org/whstsa/library/gui/factories/GuiUtils.java
+++ src/org/whstsa/library/gui/factories/GuiUtils.java
@@ -2,6 +2,7 @@ package org.whstsa.library.gui.factories;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -10,10 +11,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import org.whstsa.library.LibraryDB;
 import org.whstsa.library.gui.components.CheckBoxElement;
@@ -56,7 +54,47 @@ public class GuiUtils {
 		root.getChildren().add(label);
 		return root;
 	}
-	
+
+    public static HBox createHBox(double padding, Node ...nodes) {
+        HBox container = new HBox(nodes);
+        container.setPadding(new Insets(padding, padding, padding, padding));
+        return container;
+    }
+    public static HBox createHBox(Node ...nodes) {
+        return createHBox(5.0, nodes);
+    }
+
+    public static VBox createVBox(double padding, Node ...nodes) {
+        VBox container = new VBox(nodes);
+        container.setPadding(new Insets(padding, padding, padding, padding));
+        return container;
+    }
+
+    public static VBox createVBox(Node ...nodes) {
+        return createVBox(5.0, nodes);
+    }
+
+	public static BorderPane createBorderPane(Direction direction, Node mainNode, Node asideNode) {
+		BorderPane pane = new BorderPane();
+		pane.setCenter(mainNode);
+		switch (direction) {
+        case LEFTHAND:
+            pane.setLeft(asideNode);
+            break;
+        case RIGHTHAND:
+            pane.setRight(asideNode);
+            break;
+        default:
+            pane.setLeft(asideNode);
+            break;
+        }
+        return pane;
+	}
+
+	public static BorderPane createBorderPane(Node mainNode, Node asideNode) {
+	    return createBorderPane(Direction.LEFTHAND, mainNode, asideNode);
+    }
+
 	public static Scene createScene(Parent parent) {
 		return new Scene(parent, 512, 512);
 	}
@@ -64,6 +102,9 @@ public class GuiUtils {
 	public static Button createButton(String title, ClickHandler clickHandler) {
 		LibraryDB.LOGGER.debug("Assembling button with title " + title + " (CLICK HANDLER: " + Logger.assertion(clickHandler != null) + ")");
 		Button button = new Button(title);
+		if (title.length() < 10) {
+		    button.setPrefWidth(80.0);
+        }
 		if (clickHandler != null) {
 			button.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
@@ -111,11 +152,16 @@ public class GuiUtils {
 	    return createCheckBox(prompt, false);
     }
 
-	public static Label createLabel(String text) {
+	public static Label createLabel(String text, double size) {
 		LibraryDB.LOGGER.debug("Assembling label with text " + text);
 		Label label = new Label(text);
+		label.setFont(Font.font(size));
 		return label;
 	}
+
+	public static Label createLabel(String text) {
+	    return createLabel(text, 16);
+    }
 	
 	public static Label createTitle(String title) {
 		LibraryDB.LOGGER.debug("Assembling title label with text " + title);
@@ -124,6 +170,7 @@ public class GuiUtils {
 		return label;
 	}
 	public static void defaultCloseOperation(ActionEvent event) {
+		LibraryDB.LOGGER.debug("Window closed");
         ((Node)(event.getSource())).getScene().getWindow().hide();
 	}
 
@@ -138,8 +185,8 @@ public class GuiUtils {
 //
 //	}
 	
-	public static enum Float {
-		TOP, LEFT, BOTTOM, RIGHT;
+	public static enum Direction {
+		LEFTHAND, RIGHTHAND;
 		
 		public final String name = this.name().toLowerCase();
 	}
