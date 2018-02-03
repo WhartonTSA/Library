6ae4f18271dbf408ea80059116b7d57838781df4
Eric Rabil <ericjrabil@gmail.com>
2018-02-03 12:03:53 -0500

    Fixed the status bar by not overwriting child properties and simplified certain processes (not backgroundworker related)

--- src/org/whstsa/library/gui/api/GuiStatusBar.java
+++ src/org/whstsa/library/gui/api/GuiStatusBar.java
@@ -2,7 +2,6 @@ package org.whstsa.library.gui.api;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.ToolBar;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,6 +14,7 @@ import org.whstsa.library.World;
 import org.whstsa.library.api.BackgroundWorker;
 import org.whstsa.library.gui.components.LabelElement;
 import org.whstsa.library.gui.factories.GuiUtils;
+import org.whstsa.library.util.Logger;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -25,6 +25,12 @@ public class GuiStatusBar extends HBox {
     private LabelElement dateLabel;
     private boolean saved;
     private boolean statusOverride;//Used to override the normal status and replace with more urgent message
+
+    private String currentStatus = "";
+
+    private static final String LIBRARY_SAVED = "The library is saved.";
+    private static final String LIBRARY_UNSAVED = "The library is unsaved. Exiting will erase all progress since last save.";
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy");
 
     public GuiStatusBar() {
         Image icon = new Image("file:LibraryManagerIcon.png");
@@ -54,23 +60,21 @@ public class GuiStatusBar extends HBox {
         liveSavedStatus();
         liveDate();
     }
-    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
     private void liveDate() {
         BackgroundWorker.getBackgroundWorker().registerOperation(() -> {
-            String dateString = simpleDateFormat.format(World.getDate());
+            String dateString = SIMPLE_DATE_FORMAT.format(World.getDate());
             this.dateLabel.setText(dateString);
 
-            if (!simpleDateFormat.format(World.getDate())
-                    .equals(simpleDateFormat.format(new Date()))) {//Check if today's date matches simulated date
+            if (!SIMPLE_DATE_FORMAT.format(World.getDate())
+                    .equals(SIMPLE_DATE_FORMAT.format(new Date()))) {//Check if today's date matches simulated date
                 this.dateLabel.setTextFill(Color.web("#0056ad"));
                 this.dateLabel.setTooltip(new Tooltip("This is the date simulated by the Library Manager, not today's date."));
 
                 this.statusOverride = true;
                 this.statusLabel.setTextFill(Color.web("#0056ad"));
                 this.statusLabel.setText("The data you are seeing has been simulated. You may not want to save this data.");
-            }
-            else {
+            } else {
                 this.dateLabel.setTextFill(Color.web("#3d3d3d"));
                 this.dateLabel.setTooltip(null);
                 this.statusOverride = false;
@@ -81,13 +85,20 @@ public class GuiStatusBar extends HBox {
 
     private void liveSavedStatus() {
         BackgroundWorker.getBackgroundWorker().registerOperation(() -> {
-            if (this.statusOverride) {return;}
+            if (this.statusOverride) {
+                return;
+            }
+            String newValue = this.saved ? LIBRARY_SAVED : LIBRARY_UNSAVED;
+            if (this.statusLabel.getID().equals(newValue)) {
+                return;
+            }
             setStatusLabel(this.saved ? "The library is saved." : "The library is unsaved. Exiting will erase all progress since last save.");
         });
     }
 
     private void setStatusLabel(String newLabel) {
-        this.statusLabel = GuiUtils.createLabel(newLabel);
+        this.statusLabel.setID(newLabel);
+        this.statusLabel.setText(newLabel);
     }
 
     public void setSaved(boolean saved) {
