9fbf51874061c0bcf3026d88f704efcf046d09c2
Camnewb <camnewborn@gmail.com>
2018-02-13 11:15:15 -0500

    Added New File menu item
    Thoughtful description

--- src/org/whstsa/library/gui/api/MainMenuBar.java
+++ src/org/whstsa/library/gui/api/MainMenuBar.java
@@ -4,6 +4,7 @@ import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.scene.control.Alert;
 import javafx.scene.control.MenuBar;
 import javafx.scene.input.KeyCombination;
+import javafx.stage.FileChooser;
 import org.whstsa.library.LibraryDB;
 import org.whstsa.library.api.IPerson;
 import org.whstsa.library.api.ObservableReference;
@@ -17,6 +18,7 @@ import org.whstsa.library.gui.dialogs.*;
 import org.whstsa.library.gui.factories.DialogUtils;
 import org.whstsa.library.util.Logger;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainMenuBar {
@@ -30,15 +32,26 @@ public class MainMenuBar {
     public MainMenuBar(Table<IBook> bookTable, Table<IMember> memberTable, ObservableReference<ILibrary> libraryReference, Table<ILibrary> libraryTable, Table<IPerson> personTable, LibraryDB libraryDB, GuiStatusBar statusBar) {
 
         MenuBarElement barElement = new MenuBarElement();
-        //Ignore IDE warnings about NullPointers, they are handled
+        //Ignore IDE warnings about NullPointers, they are handled by gui
         barElement.addMenu("_File");
-        barElement.addSubMenu(0, MenuBarElement.createMenu("_New"));
-        barElement.addSubMenuItem(0, 0, "New _Person...", event -> PersonMetaDialogs.createPerson(person -> {if (personTable != null) {personTable.refresh();}}), KeyCombination.keyCombination("CTRL+P"));
-        barElement.addSubMenuItem(0, 0, "New _Library...", event -> LibraryMetaDialogs.createLibrary(library -> {if (libraryTable != null) {libraryTable.refresh();}}), KeyCombination.keyCombination("CTRL+L"));
-        barElement.addSubMenuItem(0, 0, "New _Book...", event -> BookMetaDialogs.createBook(book -> bookTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+B"), bookTable == null);
-        barElement.addSubMenuItem(0, 0, "New _Membership...", event -> MemberMetaDialogs.createMember(member -> memberTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+M"), memberTable == null);
-        barElement.addSubMenuItem(0, 0, "New _Checkout...", event -> CheckoutMetaDialogs.checkOutPreMenu(checkout -> memberTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+C"), libraryReference == null);
-        barElement.addSubMenuItem(0, 0, "New _Return...", event -> CheckoutMetaDialogs.checkInPreMenu(checkout -> memberTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+R"), libraryReference == null);
+        barElement.addMenuItem(0, "_New Data File...", event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Save New File");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON file", "json"));
+            fileChooser.setInitialDirectory(new File(libraryDB.getConfig().getProperty("initialDirectory")));
+            fileChooser.setInitialFileName(".json");
+            File file = fileChooser.showSaveDialog(libraryDB.getStage());//I'll probably make a class for this, or overhaul the IOFileSelection class
+            try {
+                if (file.createNewFile()) {
+                    LibraryDB.LOGGER.debug("File saved at path " + file.getPath());
+                }
+                else {
+                    DialogUtils.createDialog("There was an error", "File already exists.", null, Alert.AlertType.ERROR).show();
+                }
+            } catch (Exception ex) {
+                DialogUtils.createDialog("There was an error.", ex.getMessage(), null, Alert.AlertType.ERROR).show();
+            }
+        }, KeyCombination.keyCombination("CTRL+N"));
         barElement.addMenuItem(0, "Save", event -> {
             try {
                 LibraryDB.getFileDelegate().save(Loader.getLoader().computeJSON());
@@ -53,22 +66,31 @@ public class MainMenuBar {
         barElement.addMenuItem(0, "_Preferences...", event -> libraryDB.getInterfaceManager().display(new GuiPreferences(libraryDB, libraryReference)), null);
         barElement.addMenuSeparator(0);
         barElement.addMenuItem(0, "_Exit", event -> ExitMetaDialogs.exitConfirm(statusBar != null && !statusBar.getSaved()), null);
+
         barElement.addMenu("_Edit");
+        barElement.addSubMenu(1, MenuBarElement.createMenu("_Create"));
+        barElement.addSubMenuItem(1, 0, "New _Person...", event -> PersonMetaDialogs.createPerson(person -> {if (personTable != null) {personTable.refresh();}}), KeyCombination.keyCombination("CTRL+P"));
+        barElement.addSubMenuItem(1, 0, "New _Library...", event -> LibraryMetaDialogs.createLibrary(library -> {if (libraryTable != null) {libraryTable.refresh();}}), KeyCombination.keyCombination("CTRL+L"));
+        barElement.addSubMenuItem(1, 0, "New _Book...", event -> BookMetaDialogs.createBook(book -> bookTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+B"), bookTable == null);
+        barElement.addSubMenuItem(1, 0, "New _Membership...", event -> MemberMetaDialogs.createMember(member -> memberTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+M"), memberTable == null);
+        barElement.addSubMenuItem(1, 0, "New _Checkout...", event -> CheckoutMetaDialogs.checkOutPreMenu(checkout -> memberTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+C"), libraryReference == null);
+        barElement.addSubMenuItem(1, 0, "New _Return...", event -> CheckoutMetaDialogs.checkInPreMenu(checkout -> memberTable.refresh(), libraryReference), KeyCombination.keyCombination("CTRL+R"), libraryReference == null);
+        barElement.addMenuSeparator(1);
         barElement.addMenuItem(1, "_Edit JSON... (Dev)", event -> EditJSONMetaDialogs.editConfirm(libraryDB.getJsonRawFile()), null);
         barElement.addSubMenu(1, MenuBarElement.createMenu("_Simulate"));
-        barElement.addSubMenuItem(1, 1, "_Simulate Days", event -> SimulateMetaDialogs.simulateDay(days -> {
+        barElement.addSubMenuItem(1, 3, "_Simulate Days", event -> SimulateMetaDialogs.simulateDay(days -> {
             if (libraryReference != null) {
                 bookTable.refresh();
                 memberTable.refresh();
             }
         }), KeyCombination.keyCombination("CTRL+SHIFT+S"));
-        barElement.addSubMenuItem(1, 1, "Populate _Members", event -> PopulateMetaDialogs.populateMemberMenu(amount -> {
+        barElement.addSubMenuItem(1, 3, "Populate _Members", event -> PopulateMetaDialogs.populateMemberMenu(amount -> {
             if (libraryReference != null) {
                 bookTable.refresh();
                 memberTable.refresh();
             }
         }), KeyCombination.keyCombination("CTRL+SHIFT+M"));
-        barElement.addSubMenuItem(1, 1, "Populate _Books", event -> PopulateMetaDialogs.populateBookMenu(amount -> {
+        barElement.addSubMenuItem(1, 3, "Populate _Books", event -> PopulateMetaDialogs.populateBookMenu(amount -> {
             if (libraryReference != null) {
                 bookTable.refresh();
                 memberTable.refresh();
@@ -82,6 +104,7 @@ public class MainMenuBar {
             personTable.refresh();
             }
         }, KeyCombination.keyCombination("F5"));
+
         barElement.addMenu("_Help");
         barElement.addMenuItem(2, "_About...", event -> libraryDB.getInterfaceManager().display(new GuiAbout(libraryDB, libraryReference)), null);
         barElement.addMenuItem(2, "_Help...", event -> libraryDB.getInterfaceManager().display(new GuiHelp(libraryDB, libraryReference)), null);
