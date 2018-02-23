b42b285b31495604b691c8241357e417e4249a94
Eric Rabil <ericjrabil@gmail.com>
2018-02-23 09:06:19 -0800

    Converted check-box element to required

--- src/org/whstsa/library/gui/components/CheckBoxElement.java
+++ src/org/whstsa/library/gui/components/CheckBoxElement.java
@@ -5,16 +5,24 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import org.whstsa.library.gui.factories.GuiUtils;
 
-public class CheckBoxElement extends CheckBox implements Element {
+import java.util.function.Consumer;
 
-    private Label label;
+public class CheckBoxElement extends CheckBox implements RequiredElement {
+
+    private Node label;
     private String id;
+    private boolean required;
 
-    public CheckBoxElement(String id, String label, boolean inline, boolean disabled) {
+    public CheckBoxElement(String id, String label, boolean inline, boolean disabled, boolean required) {
         super();
         this.id = id;
         this.label = GuiUtils.createLabel(label, 14);
+        this.required = required;
         super.setDisable(disabled);
+    }
+
+    public CheckBoxElement(String id, String label, boolean inline, boolean disabled) {
+        this(id, label, inline, disabled, false);
     }
 
     @Override
@@ -23,6 +31,10 @@ public class CheckBoxElement extends CheckBox implements Element {
             return this;
         }
         return GuiUtils.createSplitPane(GuiUtils.Orientation.HORIZONTAL, this.label, this);
+    }
+
+    public void setLabel(Node label) {
+        this.label = label;
     }
 
     @Override
@@ -48,5 +60,25 @@ public class CheckBoxElement extends CheckBox implements Element {
     @Override
     public boolean getBoolean() {
         return this.getResult();
+    }
+
+    @Override
+    public boolean isRequired() {
+        return this.required;
+    }
+
+    @Override
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    @Override
+    public boolean isSatisfied() {
+        return this.selectedProperty().get();
+    }
+
+    @Override
+    public void setOnSatisfactionUpdate(Consumer<Boolean> onSatisfactionUpdate) {
+        this.selectedProperty().addListener((obs, oldal, newVal) -> onSatisfactionUpdate.accept(newVal));
     }
 }
